b9074ba17995df5686b73cb1d3f9e7d11bd8a6ae
Darpit Rangari <95137001+proxima424@users.noreply.github.com>
2022-12-19 21:34:51 +0530

    wow1

--- src/DNSerc20.sol
+++ src/DNSerc20.sol
@@ -1,2 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
+
+//dependencies
+import {ERC20} from
+    "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract DNSerc20 is ERC20 {
+    address immutable DNSfactory;
+
+    constructor(string memory _name, string memory _symbol, address _DNSfactory)
+        ERC20(_name, _symbol)
+    {
+        DNSfactory = _DNSfactory;
+    }
+
+    function onlyFactory() internal {
+        require(msg.sender == DNSfactory, "INVALID_CALLER");
+        _;
+    }
+
+    function mint(address _receiver, uint256 quantity) external {
+        onlyFactory();
+        _mint(_receiver, quantity);
+    }
+
+    function burn(address _receiver, uint256 quantity) external {
+        onlyFactory();
+        _burn(_receiver, _quantity);
+    }
+}
